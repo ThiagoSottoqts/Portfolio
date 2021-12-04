eb6434d0e7ae2368f2d00622aee8fe255b2ad088
Thiago Sotto <thiago.sotto@hotmail.com>
2021-12-04 11:55:15 -0300

    Iniciando a confecaçao do site e inserindo o cabeçalho

---
+++ Script.js
@@ -0,0 +1,17 @@
+//Abertura do Menu Mobile
+
+let menuMobile = document.querySelector('.menu_mobile')
+
+let open = false//flag estado inicial de fechado
+
+function abrirMobile(){
+
+    if (open === true) {
+        open = false
+        menuMobile.classList.remove('open')
+    } else if (open === false) {
+        open = true
+    menuMobile.classList.add('open')    
+} 
+}
+
