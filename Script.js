f317a15e339db41355a4b736119dc2f2195eda97
Thiago Sotto <thiago.sotto@hotmail.com>
2021-12-09 17:36:05 -0300

    Reestilização e mudança do layout

--- Script.js
+++ Script.js
@@ -1,17 +1,42 @@
-//Abertura do Menu Mobile
+class MobileNavbar {
+    constructor(mobileMenu, navList, navLinks) {
+        this.mobileMenu = document.querySelector(mobileMenu)
+        this.navList = document.querySelector(navList) 
+        this.navLinks = document.querySelectorAll(navLinks)
+        this.activeClass = "active"
 
-let menuMobile = document.querySelector('.menu_mobile')
+        this.handleClick = this.handleClick.bind(this);
+    }    
 
-let open = false//flag estado inicial de fechado
+    animateLinks() {
+        this.navLinks.forEach((link, index) => {
+            link.style.animation
+            ? (link.style.animation = "")
+            : (link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`);
+        });
+    }
 
-function abrirMobile(){
+    handleClick () {
+        this.navList.classList.toggle(this.activeClass)
+        this.animateLinks()
+    }
 
-    if (open === true) {
-        open = false
-        menuMobile.classList.remove('open')
-    } else if (open === false) {
-        open = true
-    menuMobile.classList.add('open')    
-    } 
+    addClickEvent () {
+        this.mobileMenu.addEventListener("click", this.handleClick);
+    }
+
+    init() {
+        if (this.mobileMenu) {
+            this.addClickEvent()
+        }
+        return this;
+    }
 }
 
+const mobileNavbar = new MobileNavbar(
+    ".mobile-menu",
+    ".nav-list",
+    ".nav-list li",
+);
+
+mobileNavbar.init();
